3d9a0c9857afa2b4e1b15d50cc61b73b27d5e7c1
Zikri Khairan <zikrikhairan156@gmail.com>
2022-12-08 18:08:22 +0700

    [Zikri] Create app.go

---
+++ app.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type FileInfo struct {
+	Name  string
+	IsDir bool
+	Mode  os.FileMode
+}
+
+const (
+	filePrefix = "/music/"
+	root       = "./music"
+)
+
+func main() {
+	http.HandleFunc("/", playerMainFrame)
+	http.HandleFunc(filePrefix, File)
+	err := http.ListenAndServe(":53405", nil)
+	if err != nil {
+		return
+	}
+}
+
+func playerMainFrame(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./index.html")
+}
+
+func File(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join(root, r.URL.Path[len(filePrefix):])
+	stat, err := os.Stat(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if stat.IsDir() {
+		serveDir(w, r, path)
+		return
+	}
+	http.ServeFile(w, r, path)
+}
+
+func serveDir(w http.ResponseWriter, r *http.Request, path string) {
+	defer func() {
+		if err, ok := recover().(error); ok {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}()
+	file, err := os.Open(path)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+	if err != nil {
+		log.Println(err)
+	}
+
+	files, err := file.Readdir(-1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fileinfos := make([]FileInfo, len(files), len(files))
+
+	for i := range files {
+		fileinfos[i].Name = files[i].Name()
+		fileinfos[i].IsDir = files[i].IsDir()
+		fileinfos[i].Mode = files[i].Mode()
+	}
+
+	j := json.NewEncoder(w)
+
+	if err := j.Encode(&fileinfos); err != nil {
+		log.Println(err)
+	}
+}
